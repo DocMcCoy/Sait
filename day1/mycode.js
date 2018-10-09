4045a2df728a11efa64b2a8c97c1234a45c3795b
DocMcCoy <43177751+DocMcCoy@users.noreply.github.com>
2018-10-09 10:22:25 -0600

    Created the forutne teller and the age calculator

--- day1/mycode.js
+++ day1/mycode.js
@@ -1,5 +1,20 @@
 alert('Hello World');
-console.log('Secret message') //Commenting to the console
-document.write('Secret message') //Commenting directly to the page
-document.write('Another secret message') 
-document.write('Wait ... none of these messages are Secret') 
+
+console.log('Secret message'); //Commenting to the console
+
+document.write('Secret message'); //Commenting directly to the page
+document.write('Another secret message'); 
+document.write('Wait ... none of these messages are Secret');
+
+var x = 5;
+console.log(x);
+
+var y = 7;
+var z = 10;
+console.log(y * z);
+
+var firstName = 'David';
+var lastName = 'Wakely';
+var wholeName = 'The secret name is '+ firstName + ' ' + lastName;
+console.log(wholeName);
+
