1361b19b9bce5ef5750ca1f7b93fee254dd6b619
DocMcCoy <43177751+DocMcCoy@users.noreply.github.com>
2018-10-09 08:54:26 -0600

    Added more "secret" messages

--- day1/mycode.js
+++ day1/mycode.js
@@ -1,3 +1,5 @@
 alert('Hello World');
 console.log('Secret message') //Commenting to the console
 document.write('Secret message') //Commenting directly to the page
+document.write('Another secret message') 
+document.write('Wait ... none of these messages are Secret') 
