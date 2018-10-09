56ca1bb65bb2884dd298d748533babd639e9c2b8
DocMcCoy <43177751+DocMcCoy@users.noreply.github.com>
2018-10-09 13:52:49 -0600

    Added my exercise folder

--- day1/temperaturecoat/mycode.js
+++ day1/temperaturecoat/mycode.js
@@ -1,7 +1,13 @@
 var temperature = prompt("What temperature is it?");
+var coat = prompt("Do you have a coat? - true or false")
+var hat = prompt("Do you have a hat? - true or false")
 
-if (temperature < 10) {
+if (temperature < -20 || coat || hat) {
+    document.write('Shut the door asshole. You\'re not going outside'); 
+} else if (temperature < 0  && coat == true) {
+    document.write('What are ya dumb?!? It\'s cold outside get a hat AND a coat.');
+} else if (temperature < 10 && coat == true) {
     document.write('Put on a coat ya dummy');
 } else {
-    document.write('No need for a coat today');
+    document.write('Naked? Bold choice but at least it shouldn\'t freeze off');
 }
