b8446ed826f24f0a78d226057443890c373a3fc2
DocMcCoy <43177751+DocMcCoy@users.noreply.github.com>
2018-10-15 15:04:25 -0600

    Added some code to the exercise

--- day5/exercise/js/code.js
+++ day5/exercise/js/code.js
@@ -1,20 +1,77 @@
 console.log("Linked!")
 // Assign your submit button to a variable
-var submitButton = document.myForm.submit;
+var submitBtn = document.myForm.submit;
 // Add a "click" event listener to your submitButton
-console.log(submitButton)
+console.log(submitBtn)
+
+var submitButton = document.getElementById('submit');
+var errorQuiz1 = document.getElementById('errorQuiz1');
+var errorQuiz2 = document.getElementById('errorQuiz2');
+var errorQuiz3 = document.getElementById('errorQuiz3');
+var errorQuiz4 = document.getElementById('errorQuiz4');
+var errorQuiz5 = document.getElementById('errorQuiz5');
+
 submitButton.addEventListener("click", function(event) {
-// Prevent the default action
-event.preventDefault();
-// Run the rest of your code
-var quiz1value = document.myForm.quiz1.value;
-console.log(quiz1value);
-var quiz2value = document.myForm.quiz2.value;
-console.log(quiz2value);
-var quiz3value = document.myForm.quiz3.value;
-console.log(quiz3value);
-var quiz4value = document.myForm.quiz4.value;
-console.log(quiz4value);
-var quiz5value = document.myForm.quiz5.value;
-console.log(quiz5value);
-})
+    // Code goes here
+    errorQuiz1.style.display = "none";
+    errorQuiz2.style.display = "none";
+    errorQuiz3.style.display = "none";
+    errorQuiz4.style.display = "none";
+    errorQuiz5.style.display = "none";
+    
+    var quiz1 = document.myForm.quiz1.value;
+    console.log(quiz1)
+    var quiz2 = document.myForm.quiz2.value;
+    console.log(quiz2)
+    var quiz3 = document.myForm.quiz3.value;
+    console.log(quiz3)
+    var quiz4 = document.myForm.quiz4.value;
+    console.log(quiz4)
+    var quiz5 = document.myForm.quiz5.value;
+    console.log(quiz5)
+
+    if (!quiz1) {
+        event.preventDefault();
+        errorQuiz1.style.display = "block";
+    }
+    if (!quiz2) {
+        event.preventDefault();
+        errorQuiz2.style.display = "block";
+    }
+    if (!quiz3) {
+        event.preventDefault();
+        errorQuiz3.style.display = "block";
+    }
+    if (!quiz4) {
+        event.preventDefault();
+        errorQuiz4.style.display = "block";
+    }
+    if (!quiz5) {
+        event.preventDefault();
+        errorQuiz5.style.display = "block";
+    }
+});
+
+// http://jsfiddle.net/dY372/
+
+
+document.getElementById("answer").onclick = validate;
+
+function validate() {
+    var checkboxes = document.getElementsByName("input");
+    var checkboxChecked = []; 
+
+    for(var i = 0; i < checkboxes.length; i++) {
+        if(checkboxes[i].checked && (checkboxes[i].value === "right")) {
+            checkboxChecked.push(checkboxes[i]);
+        }
+    }
+
+    if(checkboxChecked.length === 2) {
+        alert("You answered correctly");
+    } 
+    else {
+        alert("Wrong answer");
+    }
+}
+
