ff1243d4129d93eaae55b265c24a163a30196165
DocMcCoy <43177751+DocMcCoy@users.noreply.github.com>
2018-10-15 08:50:20 -0600

    Added some Form stuff

---
+++ day5/activities/form1/js/mycode.js
@@ -0,0 +1,10 @@
+console.log("this page is linked!")
+
+var myForm = document.myForm;
+console.log(myForm)
+
+var fname = document.myForm.firstname;
+console.log(fname)
+
+var fnameValue = document.myForm.firstname.value;
+console.log(fnameValue)
