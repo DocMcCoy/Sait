dc8160a0ed56a8108c3c7ee1adf4d3eccac44616
DocMcCoy <43177751+DocMcCoy@users.noreply.github.com>
2018-10-16 20:39:43 -0600

    added an icon thing to the page to animate later

--- day6/exercise/js/code.js
+++ day6/exercise/js/code.js
@@ -4,17 +4,19 @@ random = Math.random()
 var colorstest = randomColor(Math.round(Math.random() * 20));
 console.log("Random Number of Colour Array = " + colorstest)
 
-
+ClipboardJS.isSupported();
 
 $('.congrats').fadeIn("slow");
-// Color Options:
-// spec­ify the hue, lu­mi­nos­ity and the num­ber of col­ors to gen­er­ate
 
 $(document).ready(function () {
     $('input[type=submit]').css({ 'background-color': randomColor({ hue: "random", luminosity: "dark" }) });
     $('input[type=submit]').click(function () {
         event.preventDefault();
         $("#congrats").removeClass("invisible").addClass("congrats");
+        $("#pointer").removeClass("imgContainer").addClass("imgContainerInvisible");
+
+        // Color Options:
+        // spec­ify the hue, lu­mi­nos­ity and the num­ber of col­ors to gen­er­ate
         var hue = $('input[name=hue]:checked').val();
         console.log(hue);
         var luminosity = $('input[name=luminosity]:checked').val();
@@ -25,29 +27,27 @@ $(document).ready(function () {
         var colors = randomColor({ hue: hue, luminosity: luminosity, count: colorNumber });
         console.log(colors)
         $("#pallete").empty();
-        // Use the jQuery each method to loop through each link.
+
+        // Use the jQuery each method to loop through each of the color array.
         $.each(colors, function (index, value) {
             var color = value;
             $('input[type=submit]').css({ 'background-color': randomColor({ hue: "random", luminosity: "dark" }) });
-            $('#pallete').append('<div class="roundDiv" data-clipboard-target="' + color + '" style="color: ' + color + '; background: ' + color + '">\\' + color + '</div>')
-
+            $('#pallete').append('<div class="roundDiv" data-clipboard-target="' + color + '" style="color: ' + color + '; background: ' + color + '">' + color + '</div>')
             console.log(color);
-
         });
-
-
     });
-
-
 });
 
-$('.roundDiv').click(function () {
-var divColor = document.getElementById('roundDiv');
-var clipboard = new ClipboardJS(divColor);
-clipboard.on('success', function (e) {
-    console.log(e);
-});
-clipboard.on('error', function (e) {
-    console.log(e);
-});
-});
+
+// Click to copy to clipboard ... no worky
+
+// $( ".roundDiv" ).click(function() {
+//     var divColor = document.getElementById('roundDiv');
+//     var clipboard = new ClipboardJS(divColor);
+//     clipboard.on('success', function (e) {
+//         console.log(e);
+//     });
+//     clipboard.on('error', function (e) {
+//         console.log(e);
+//     });
+// });
